b0723a83d5d318cac2b53baecfc5d15ed7d7f191
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-06 16:44:07 +0200

    migliora lo stile delle cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,19 @@
 export default function ProductCard(prop) {
-     const {image, title} = prop.productProp;
-    return(
-        
-
-        <li><div className="product-card">
-            <div className="card-img">
-              <img src={image}></img>  
+    const { image, title, price, description, category, rating } = prop.productProp;
+    return (
+        <li>
+            <div className="product-card">
+                <div className="card-img">
+                    <img src={image}></img>
+                </div>
+                <div className="card-text">
+                    <h3 className="big-text">{title}</h3>
+                    <p className="price-label">{price.toFixed(2)}€</p>
+                    <p><strong>{category.toUpperCase()}</strong></p>
+                    <p className="small-text">{description}</p>
+                    <p>{rating.rate}{rating.count}</p>
+                </div>
             </div>
-            <span>{title}</span>
-        </div></li>
+        </li>
     )
 }
