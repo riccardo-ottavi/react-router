c0473e0ad7ab2d40146e55e371f847a48ec4a27e
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-06 15:30:37 +0200

    inizializza axios

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,18 @@
+
+
 export default function Products() {
+
+    function fetchProduct() {
+        const endPoint = "https://fakestoreapi.com/products"
+        axios.get(endPoint)
+            .then((res =>
+                console.log("chiamata riuscita: ", res))
+            ).catch(error => console.log(error)
+            )
+    }
+    
+    fetchProduct();
+
     return(
         <header>
             <div className="section-title">
