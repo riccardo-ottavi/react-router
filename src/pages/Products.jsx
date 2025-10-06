e029e972e340ffe9fe279954019123415cb3449a
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-06 19:33:44 +0200

    correggi l'evento filtro

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -31,7 +31,7 @@ export default function Products() {
         } else {
             setFilteredProducts(products)
         }
-    }, [selectedCategory]);
+    }, [selectedCategory, products]);
 
     return (
         <>  
